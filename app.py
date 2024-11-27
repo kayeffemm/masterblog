52bab74e0da7da53b82a1647392c94a63bdf9ca7
kayeffemm <kenneth.meier95@gmail.com>
2024-11-27 16:00:22 +0100

    create index route

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from storage.json_reader import load_blog_posts
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_blog_posts()
+    return render_template('index.html', posts = blog_posts)
 
 
 if __name__ == '__main__':
--- storage/json_reader.py
+++ storage/json_reader.py
@@ -6,7 +6,7 @@ def load_blog_posts() -> list:
     Loads the blog_posts.json file and returns content as a list of dictionaries.
     :return: list[dictionaries]
     """
-    with open('../data/blog_posts.json', "r") as handle:
+    with open('./data/blog_posts.json', "r") as handle:
         data = json.load(handle)
     return data
 
